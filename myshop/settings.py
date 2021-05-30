af4e9b3d7cdc59d376dd0c1a8effd4bf44e56015
alimov <alikce.2001@gmail.com>
2021-05-30 19:58:19 +0300

    sdfsd

--- myshop/settings.py
+++ myshop/settings.py
@@ -133,7 +133,7 @@ DATABASES = {
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
-
+#fdsf
 #DATABASES = {
  #   'default': dj_database_url.config()
 #}
